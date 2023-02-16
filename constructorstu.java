6f3825a1c077b76210a5ef6f0154236d53da9904
hrishikasingh2001 <78427129+hrishikasingh2001@users.noreply.github.com>
2023-02-16 19:13:24 +0530

    Add files via upload

---
+++ constructorstu.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class print
+{
+print(int a,String b)
+{
+System.out.print(a);
+System.out.print(b);
+}
+public static void main(String args[])
+{
+print obj= new print(12,"Hrishika");
+print obj2=new print(\n13,"Diksha");
+print obj3=new print(\n14,"Garima");
+print obj4=new print(\n15,"Prashnsa");
+}
+}
